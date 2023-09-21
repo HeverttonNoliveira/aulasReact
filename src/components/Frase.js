e294aa1b2c728730ea73aa406bff76ad011215c3
HeverttonNoliveira <oliveirahn.nunes@gmail.com>
2023-09-21 08:58:15 -0300

    primeior commit

---
+++ src/components/Frase.js
@@ -0,0 +1,9 @@
+function Frase() {
+    let nome = "x"
+
+    return(
+        <p> Olá meu nome é {nome}</p>
+    )
+}
+
+export default Frase
---
+++ src/components/TituloInicial.js
@@ -0,0 +1,12 @@
+import Frase from "./Frase"
+
+function TituloInicial() {
+    return(
+        <div>
+            <h1>Esse é meu Primeiro Projeto em React</h1>
+            <Frase/>
+        </div>
+    )
+}
+
+export default TituloInicial
